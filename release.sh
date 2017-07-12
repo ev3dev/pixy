32331c62c4f200d37e2a630d4de8dfbe5a626c8b
David Lechner <david@lechnology.com>
2017-07-12 13:40:14 -0500

    ev3dev-trusty

--- release.sh
+++ release.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
-#
-# Maintainer script for publishing releases.
 
 set -e
 
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
 
-OS=debian DIST=stretch ARCH=amd64 pbuilder-ev3dev build
-OS=debian DIST=stretch ARCH=i386 PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
-OS=debian DIST=stretch ARCH=armhf PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
-OS=debian DIST=stretch ARCH=armel PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
-OS=raspbian DIST=stretch ARCH=armhf pbuilder-ev3dev build
+debuild -S
 
-debsign ~/pbuilder-ev3dev/debian/stretch-amd64/${source}_${version}_amd64.changes
-debsign ~/pbuilder-ev3dev/debian/stretch-i386/${source}_${version}_i386.changes
-debsign ~/pbuilder-ev3dev/debian/stretch-armhf/${source}_${version}_armhf.changes
-debsign ~/pbuilder-ev3dev/debian/stretch-armel/${source}_${version}_armel.changes
-debsign ~/pbuilder-ev3dev/raspbian/stretch-armhf/${source}_${version}_armhf.changes
-
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/stretch-amd64/${source}_${version}_amd64.changes
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/stretch-i386/${source}_${version}_i386.changes
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/stretch-armhf/${source}_${version}_armhf.changes
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/stretch-armel/${source}_${version}_armel.changes
-dput ev3dev-raspbian ~/pbuilder-ev3dev/raspbian/stretch-armhf/${source}_${version}_armhf.changes
+dput ppa:ev3dev/tools ../${source}_${version}_source.changes
 
 gbp buildpackage --git-tag-only
